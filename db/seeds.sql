bbedd783878f427505def4fd37b2715bebdfc1a6
Millie <millie_tsai@me.com>
2021-11-06 15:06:31 -0700

    upload basic files

---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO  recipes (id, dishName, ingredients, directions, nutrition_facts)
+VALUES (001, "Gyoza", "Pork, Green Onion, Ginger, Sauce", "Make dough, make filling, add filling to dough and close"),
---
+++ seeds/seed.js
@@ -0,0 +1,28 @@
+const sequelize = require('../config/connection');
+const { User } = require('../models');
+
+const userData = require('./userData.json');
+const recipeData = require('./recipeData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await Recipe.bulkCreate(recipeData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  process.exit(0);
+};
+
+seedDatabase();
+
+
+function addRecipe(req, res) {
+  
+}
