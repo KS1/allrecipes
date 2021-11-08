a64303599dc439595052dde478a55c1f733f49e8
Millie <millie_tsai@me.com>
2021-11-07 22:40:17 -0800

    added html,css, seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,6 @@
 INSERT INTO  recipes (id, dishName, ingredients, directions, nutrition_facts)
 VALUES (001, "Gyoza", "Pork, Green Onion, Ginger, Sauce", "Make dough, make filling, add filling to dough and close"),
+(002, "Lemony Potatoes With Halloumi Cheese", ""),
+(003, "Asparagus, Mushroom, Leek, and Cheese Galette", ""),
+(004, "Carciofi alla Romana", "")
+(005, "Asparagus Goes Deluxe With Miso Béarnaise", "");
