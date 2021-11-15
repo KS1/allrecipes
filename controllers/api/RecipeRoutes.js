dd59c19ecf3f57bbc8a57bc2f7fce936e99b7202
Millie <millie_tsai@me.com>
2021-11-14 22:06:03 -0800

    updaed signup handlebars, index.js in model and recupeRoutes.js in api/colltrollers

--- controllers/api/RecipeRoutes.js
+++ controllers/api/RecipeRoutes.js
@@ -1,42 +1,5 @@
 const router = require('express').Router();
 const Recipe = require('../models/Recipe');
-// Here is where we provide hardcoded data to render dynamically
-// const dishes = [
-//   {
-//     dish: 'French Bread with Brie Cheese',
-//     ingredients: ["French baguette with warm brie"],
-//     directions:["step1, step2, step3"],
-//     nutrition_facts:"good for you"
-//   },
-//   {
-//     dish_name: 'Cheese Plate with Spanish Chorizo',
-//     description:
-//       'Manchego, Iberico, Cabrales, fig jam, grapes, pecans, and Spanich Chorizo',
-//   },
-//   {
-//     dish_name: 'Fish Tacos',
-//     description:
-//       'Battered/fried fish, corn tortillas, fresh slaw with jalepenos and cilantro, pickled red onion',
-//   },
-//   {
-//     dish_name: 'Tropical Fruit Salad',
-//     description: 'Guava, papaya, pineapple, mango, and star fruit',
-//   },
-//   {
-//     dish_name: 'Pork Gyoza',
-//     description:
-//       'Homemade japanese dumplings stuffed with a pork and green onion filling',
-//   },
-//   {
-//     dish_name: 'Yebeg Tibs with Injera Bread',
-//     description:
-//       'Marinated lamb dish with rosemary, garlic, onion, tomato, jalapeño and the East African spice berbere',
-//   },
-//   {
-//     dish_name: 'Cape Malay Curry',
-//     description: 'Chicken and vegitable curry dish with basmati rice',
-//   },
-// ];
 
 
 // route to get all recipes
--- models/index.js
+++ models/index.js
@@ -3,10 +3,14 @@ const User = require('./User');
 const Recipe = require('./Recipe');
 // const Painting = require('./Painting');
 
-// ?? create userRecipe model?
-const UserRecipe = require ('./UserRecipe');
-UserRecipe.belongsTo(User, {
-  foreignKey: 'recipe_id',
+// ???
+User.hasMany(Recipe, {
+  foreignKey:'user_id',
+  onDelete:'CASCADE'
+});
+
+Recipe.belongsTo(User, {
+  foreignKey: 'user_id',
 });
 
 module.exports = { User, Recipe };
